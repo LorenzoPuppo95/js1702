804cf8fbf63d3c29c0565bed633dc452f5740647
LorenzoPuppo95 <puppolorenzo@live.it>
2025-02-22 00:23:05 +0100

    update esercizi casa

--- esercizi2102.js
+++ esercizi2102.js
@@ -96,13 +96,74 @@ console.log(etaMediaValue);
 
 //esercizio 3: scrivete una funzione che restituisca un array di stringhe
 //ogni stringa dovrà avere questo formato "nome/cognome"
+function getNameSurnameArray(students) {
+    let nameSurnameArray = [];
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
+        nameSurnameArray.push(`${student.name}/${student.surname}`);
+    }
+    return nameSurnameArray;
+}
+const nameSurnameArray = getNameSurnameArray(students);
+console.log(nameSurnameArray);
 
+//esercizio 4: scrivete una funzione che restituisca la somma dei maschi, delle femmine e degli eventuali altri gender
+function countGenders(students) {
+    let genderCounts = {};
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
+        const gender = student.gender;
+        if (genderCounts[gender]) {
+            genderCounts[gender]++;
+        } else {
+            genderCounts[gender] = 1;
+        }
+    }
+    return genderCounts;
+}
+const genderCounts = countGenders(students);
+console.log('somma dei maschi, delle femmine e degli eventuali altri gender', genderCounts);
 
-//esercizio 4: scrivete una funzione che restituisca la somma dei maschi e delle femmine
-
-//esercizio 5: scrivete una funzione che restutisca la media delle medie degli studenti
+//esercizio 5: scrivete una funzione che restutisca la media delle medie voti degli studenti
+function mediaDelleMedie(students) {
+    let totalAverage = 0;
+    let individualAverages = [];
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
+        const marks = student.marks;
+        const sumOfMarks = marks.reduce((acc, mark) => acc + mark, 0);
+        const averageMarks = sumOfMarks / marks.length;
+        individualAverages.push(Math.round(averageMarks * 10) / 10);
+        totalAverage += averageMarks;
+    }
+    const overallAverage = totalAverage / students.length;
+    return {
+        overallAverage: Math.round(overallAverage * 10) / 10,
+        individualAverages: individualAverages
+    };
+}
+const mediaDelleMedieResult = mediaDelleMedie(students);
+console.log('media delle medie voti degli studenti', mediaDelleMedieResult.overallAverage);
+console.log('media di ciascuno studente', mediaDelleMedieResult.individualAverages);
 
 //esercizio 6: scrivete una funzione che restituisca un oggetto così sturtturato:
 //{nationality1{student1Name, student2Name...}, nationality2{student1Name, student2Name...}}
+function groupByNationality(students) {
+    let nationalityGroups = {};
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
+        const nationality = student.nationality;
+        const name = student.name;
+        if (nationalityGroups[nationality]) {
+            nationalityGroups[nationality].push(name);
+        } else {
+            nationalityGroups[nationality] = [name];
+        }
+    }
+    return nationalityGroups;
+}
+const groupedByNationality = groupByNationality(students);
+console.log(groupedByNationality);
 
-// esercizio 7: replica https://jessbayer.com
+
+// esercizio 7: replica https://jessbayer.com  
